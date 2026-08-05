29203ca04aaa107916d888cae3790b4bc4d0927d
SATYA-1708 <bhagavansunkara12@gmail.com>
2026-08-05 12:58:46 +0530

    Fix main.tsx render syntax; update delivery price to ₹99 and FAQ

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  {/* React StrictMode will cause double mounting of components in development (effects run twice). This does not affect production builds. */}
   <StrictMode>
     <App />
   </StrictMode>
